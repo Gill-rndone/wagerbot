17daac0f14daeadc7381694a712050eba9640cbe
Dwight Acol <acoldwight@Gmail.com>
2026-05-04 15:58:06 -0400

    still working on implementing database creation script

--- db-init.py
+++ db-init.py
@@ -1,1 +1,16 @@
 import os
+import sqlite3
+import pathlib
+
+
+db_check = pathlib.Path("wager.db")
+
+if db_check.exists():
+    print("already exists")
+else:
+    # create db
+    cur = sqlite3.connect("wager.db")
+
+    with open("wager-db.sql") as fp:
+        cur.executescript(fp.read())
+        print("db created and initialized\n")
---
+++ wager-db.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "bets"(
+"id" INTEGER AUTO_INCREMENT PRIMARY KEY,
+"discord_server_id" INTEGER,
+"amount_a" INTEGER,
+"amount_b" INTEGER,
+"discord_user_id_a" INTEGER,
+"discord_user_id_b" INTEGER,
+"winner_discord_user_id" INT,
+"status" TEXT,
+);
+
+CREATE TABLE "users"(
+"discord_user_id" INTEGER NOT NULL PRIMARY KEY,
+"balance" INTEGER,
+"wins" INTEGER,
+"losses" INTEGER,
+);
