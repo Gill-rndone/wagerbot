df901e57a70b54238e958d2ab35822a7021ac257
Dwight Acol <acoldwight@Gmail.com>
2026-05-20 22:51:57 -0400

    idk nothing works

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+import dbHelpers 
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    global_top_ten = [{"position": 1,"username": charlie,"balance": "$65"}, {"position": 2, "username": "bob", "balance": "$35"}, {"position": 3, "username": "steve", "balance": "$10"}]
+    return render_template("home.html", global_top_ten = global_top_ten)
 
